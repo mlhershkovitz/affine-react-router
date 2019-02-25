3889789ae237b05b166054eb6e4d1460f485ebc5
Dev Jana <dev@primeacademy.io>
2019-02-25 15:36:33 -0600

    first lecture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Animals from './Components/Animals/Animals.js'
 import Plants from './Components/Plants/Plants.js'
@@ -9,7 +10,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       
+       <h1>React Router Affine Lecture</h1>
+       <HashRouter>
+         <div>
+           <ul>
+             <li><a href="#/">Home</a></li>
+             <li><a href="#/plants">Plants</a></li>
+             <li><a href="#/animals">Animals</a></li>
+           </ul>
+          <Route exact path="/" component={ Home } />
+          <Route path="/plants" component={ Plants }/>
+          <Route path="/animals" component={ Animals }/>
+         </div>
+       </HashRouter>
       </div>
     );
   }
